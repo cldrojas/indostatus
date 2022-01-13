a8831658e00091a34a7805a2d3e750d46b44374c
Daniel Rojas <cld.rojasz@gmail.com>
2022-01-13 20:30:55 -0300

    add router for generos entity

---
+++ api/src/generos/generos.router.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const GenerosService = require('./generos.service')
+
+const router = express.Router()
+const service = new GenerosService()
+
+router.get('/', async (req, res) => {
+  const generos = await service.find()
+  res.status(200).json(generos)
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const genero = await service.findOne(id)
+  res.status(200).json(genero)
+})
+
+router.get('/deleted', async (req, res) => {
+  const generos = await service.find()
+  const generosDeleted = generos.filter((genero) => genero.deleted)
+  res.status(200).json(generosDeleted)
+})
+
+router.post('/', async (req, res) => {
+  const { name } = req.body
+  const genero = await service.create({ name })
+  res.status(201).json(genero)
+})
+module.exports = router
